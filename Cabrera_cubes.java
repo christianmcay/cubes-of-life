33f8d99b1714af3ab4efece4fdeaec28c39a58ff
christianmcay <73971223+christianmcay@users.noreply.github.com>
2021-02-06 15:14:56 -0500

    Create Cabrera_cubes.java

---
+++ Cabrera_cubes.java
@@ -0,0 +1,31 @@
+public class Cabrera_cubes {
+    public int cubenumlength(int a, int b)
+    {
+        int result1;
+                result1 = a / b;
+        return result1;
+    }
+    
+    public int cubenumwidth(int a, int b)
+    { 
+        int result2;
+                result2 = a / b;
+        return result2;
+    }
+    
+    public int cubenumheight(int a, int b)
+    {
+        int result3;
+                result3 = a / b;
+        return result3;
+    }
+    
+    public int maxnumofcubes(int a, int b, int c)
+    {
+        int total_num_of_cubes;
+            total_num_of_cubes = a * b * c;
+        return total_num_of_cubes;
+    }
+    
+    
+}
